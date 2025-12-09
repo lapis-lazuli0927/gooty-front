60e28b4445cae9906562c991980105f582d53de1
lapis-lazuli0927 <r09270441@gmail.com>
2025-12-09 19:18:15 +0900

    feat: modalの追加

--- src/app/shops/page.tsx
+++ src/app/shops/page.tsx
@@ -4,12 +4,14 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { fetchShops, Shops } from "@/lib/api";
 import ShopCard from "@/app/components/ShopCard";
+import InputModal from "@/app/components/inputmodal";
 import styles from "./page.module.css";
 
 export default function ShopsPage() {
   const [shops, setShops] = useState<Shops[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -27,8 +29,18 @@ export default function ShopsPage() {
     loadShops();
   }, []);
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      setIsModalOpen(false);
+    }
+  };
+
   return (
     <div className={styles.shop_index_page_container}>
+      {isModalOpen && <InputModal close={closeModal} />}
       <div className={styles.header_container}>
         <div className={styles.header}>
           <h1 className={styles.header_logo}>Gooty</h1>
@@ -53,6 +65,7 @@ export default function ShopsPage() {
             className={styles.add_shop_btn_icon}
             src="/icons/add_shop_btn.svg"
             alt="/"
+            onClick={openModal}
           />
         </div>
       </div>
