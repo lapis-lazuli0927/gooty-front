0a85f43d160a6458f04632c98a4b52f0cb707494
lapis-lazuli0927 <r09270441@gmail.com>
2025-12-09 15:42:03 +0900

    feat: ショップカードのコンポーネント作成

--- src/app/components/ShopCard/index.tsx
+++ src/app/components/ShopCard/index.tsx
@@ -1,13 +1,59 @@
-import { Shops } from '@/lib/api';
+import { Shops } from "@/lib/api";
+import styles from "./index.module.css";
 
 interface ShopCardProps {
-    shop: Shops;
+  shop: Shops;
 }
 
 export default function ShopCard({ shop }: ShopCardProps) {
-    return (
-        <div>
-            <h3>{shop.name}</h3>
+  const currentLevel = shop.review !== null ? shop.review : 0;
+  const MAX_STARS = 5;
+  return (
+    <div>
+      <div className={styles.shop_card}>
+        <div className={styles.shop_accent_border}></div>
+        <div className={styles.shop_content}>
+          <div className={styles.shop_Instagram_icon}>
+            <img
+              src="/icons/shop_Instagram_icon.svg"
+              alt="Instagramのアイコン"
+            />
+          </div>
+          <div className={styles.shop_name}>
+            <p className={styles.shop_text}>{shop.name}</p>
+            <div className={styles.shop_train}>
+              <img
+                className={styles.shop_train_icon}
+                src="/icons/shop_train_icon.svg"
+                alt="/"
+              />
+              <p className={styles.shop_train_text}>{shop.station_name}</p>
+            </div>
+          </div>
+          <div className={styles.review}>
+            {[...Array(MAX_STARS)].map((_, index) => {
+              // 星の番号 (1, 2, 3, 4, 5)
+              const starNumber = index + 1;
+
+              // 塗りつぶすかどうかの判定: starNumberが currentLevel以下なら true
+              const isFilled = starNumber <= currentLevel;
+
+              return (
+                <img
+                  key={index}
+                  className={styles.review_icon}
+                  src={
+                    isFilled
+                      ? "/icons/review_star.svg" // 黄色い星 (塗りつぶし)
+                      : "/icons/review_star_empty.svg" // 無色の星 (空)
+                  }
+                  alt={`評価星 ${starNumber} (${isFilled ? "満点" : "空"})`}
+                />
+              );
+            })}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
