4e073daea983f61ea05b67fc53b8289e0dd3fffa
lapis-lazuli0927 <r09270441@gmail.com>
2026-01-23 16:28:14 +0900

    feat: headerロゴのクリックで一覧ページに遷移

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import styles from "./index.module.css";
 
 export default function Header() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/shops");
+  };
+
   return (
     <header className={styles.header}>
-      <h1 className={styles.header_logo}>Gooty</h1>
+      <h1 className={styles.header_logo} onClick={handleClick}>
+        Gooty
+      </h1>
     </header>
   );
 }
